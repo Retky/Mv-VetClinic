b1d55f9454df75ee06fffebdb0331a28ebcd4a5e
Retky <jose.arredondo.a@outlook.com>
2022-05-31 16:04:07 -0500

    Add 'querie' for questions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,26 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+----------
+-- How many animals are there?
+SELECT count(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT count(*) FROM animals WHERE escape_attempts <= 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT * FROM animals WHERE escape_attempts = (
+  SELECT MAX(escape_attempts) FROM animals
+);
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth >= '1990-01-01' AND date_of_birth < '2000-12-31'
+GROUP BY species;
