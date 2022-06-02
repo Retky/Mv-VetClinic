36ece86b696cae2142c19e3ccf9a3efbf5eafbaf
Retky <jose.arredondo.a@outlook.com>
2022-06-02 15:51:06 -0500

    Add 'visits table'

--- schema.sql
+++ schema.sql
@@ -48,3 +48,12 @@ CREATE TABLE specializations (
   CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
   CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
 );
+
+CREATE TABLE visits (
+  animals_id	INT,
+  vets_id	INT,
+  date		DATE,
+  PRIMARY KEY (animals_id, vets_id),
+  CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
