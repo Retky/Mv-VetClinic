fba24d0362b0d661969afbbd72599dc50cef3c3d
Retky <jose.arredondo.a@outlook.com>
2022-06-02 15:50:45 -0500

    Add 'specializations table'

--- schema.sql
+++ schema.sql
@@ -40,3 +40,11 @@ CREATE TABLE vets (
   date_of_graduation	DATE,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE specializations (
+  vets_id	INT,
+  species_id	INT,
+  PRIMARY KEY (vets_id, species_id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
+);
